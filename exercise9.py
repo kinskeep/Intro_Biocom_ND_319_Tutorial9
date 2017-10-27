17be60ada0694a4e9bc74f975dd83bc83d924873
Katherine <bookaholic214@gmail.com>
2017-10-27 11:20:18 -0400

    Added subsets for Q1

--- exercise9.py
+++ exercise9.py
@@ -1,6 +1,17 @@
 #Question 1
-
-
+import pandas
+import scipy
+ponzr = pandas.read_csv("ponzr1.csv")
+#subset WT and mutation 1
+subset1 = ponzr.loc[ponzr.mutation.isin(['WT','M124K']),:]
+#subset WT and mutation 2
+subset2 = ponzr.loc[ponzr.mutation.isin(['WT','V456D']),:]
+#subset WT and mutation 3
+subset3 = ponzr.loc[ponzr.mutation.isin(['WT','I213N']),:]
+initialGuess=numpy.array([1,1,1])
+fitNull=minimize(fun1a,initialGuess,method="Nelder-Mead",options={'disp':True},args=q1)
+fitAlter=minimize(fun1b,intialGuess,method="Nelder-Mead",options={'disp':True},args=q1)
+1-scipy.stats.chi2.cdf(x=D,df=1)
 
 
 #Question 2#
