b28c1d2caaf5898b571417744858e0a8be681944
Katherine <bookaholic214@gmail.com>
2017-11-01 20:34:22 -0400

    Added first half of Question 3

--- exercise9.py
+++ exercise9.py
@@ -7,7 +7,7 @@ from scipy import stats
 from scipy.stats import norm
 import numpy
 from plotnine import *
-#note: used cygwin to change mutation names to numbers
+#note: used sed in cygwin to change mutation names to numbers
 #WT=0, M124K=1, V456D=2, I213N=3
 ponzr = pandas.read_csv("ponzr1.csv")
 #subset WT and mutation 1
@@ -48,7 +48,6 @@ D=2*(fitNull.fun-fitAlter.fun)
 #resulting p-value M124K
 1-scipy.stats.chi2.cdf(x=D,df=1)
 
-
 #minimizing nll for V456D
 initialGuess=numpy.array([2000,300])
 alterGuess=numpy.array([2000,-55,300])
@@ -78,3 +77,53 @@ print(D)
 #Question 2#
 import pandas
 import scipy
+
+
+#####Question 3#####
+#load packages
+import pandas
+import scipy
+from scipy import optimize
+from scipy import stats
+from scipy.stats import norm
+import numpy
+from plotnine import *
+#load dataset
+leafy = pandas.read_csv("leafDecomp.csv")
+
+#define custom likelihood functions
+def constant(p,obs):
+    B0=p[0]
+    sigma=p[1]
+    
+    expected=B0
+    nll= -1*scipy.stats.norm(expected,sigma).logpdf(obs.decomp).sum()
+    return nll
+    
+#alternate hypotheses
+def linear(p,obs):
+    B0=p[0]
+    B1=p[1]
+    sigma=p[2]
+    
+    expected=B0+B1*obs.Ms
+    nll= -1*scipy.stats.norm(expected,sigma).logpdf(obs.decomp).sum()
+    return nll
+def humped(p,obs):
+    B0=p[0]
+    B1=p[1]
+    B2=p[2]
+    sigma=p[3]
+    
+    expected=B0+B1*obs.Ms+B2*(obs.Ms)^2
+    nll= -1*scipy.stats.norm(expected,sigma).logpdf(obs.decomp).sum()
+    return nll
+
+#parameter estimates
+###NOTE TO MATI- HUMPED FUNCTION DOESN'T WORK CURRENTLY####
+constantGuess=numpy.array([1,1])
+linearGuess=numpy.array([1,1,1])
+humpedGuess=numpy.array([200,10,-.2,1])
+fitConstant=scipy.optimize.minimize(constant,constantGuess,method="Nelder-Mead",options={'disp':True},args=leafy)
+fitLinear=scipy.optimize.minimize(linear,linearGuess,method="Nelder-Mead",options={'disp':True},args=leafy)
+fitHumped=scipy.optimize.minimize(humped,humpedGuess,method="Nelder-Mead",options={'disp':True},args=leafy)
